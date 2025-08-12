2a2e8e3bea3d221ba7c9e602764686e7ae1458b8
gabrielatakeda <gabrielatakedaa@gmail.com>
2025-08-12 19:39:23 -0300

    first commit

---
+++ iot/src/test/java/com/example/iot/IoTManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoTManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
