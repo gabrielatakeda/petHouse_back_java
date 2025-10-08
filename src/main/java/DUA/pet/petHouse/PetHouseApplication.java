17bd0ad891f517cce25a41ae3477d182b830e945
gabrielatakeda <gabrielatakedaa@gmail.com>
2025-10-08 20:26:28 -0300

    adicionando a pasta de teste que tinha sumido

--- src/main/java/DUA/pet/petHouse/PetHouseApplication.java
+++ src/main/java/DUA/pet/petHouse/PetHouseApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IoTManagerApplication {
+public class PetHouseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IoTManagerApplication.class, args);
+		SpringApplication.run(PetHouseApplication.class, args);
 	}
 
 }
